d324ce350aceecc9e935298a5a65616752d0a38d
amr mohamed <amrmohamed09@gmail.com>
2023-12-30 22:35:42 +0200

    adding project styles

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { ImArrowUpRight2 } from "react-icons/im";
 import { Link } from "react-router-dom";
+import { styles } from "./projectStyles";
 
 export default function Projects() {
   const responsive = {
@@ -26,13 +27,13 @@ export default function Projects() {
   };
   return (
     <div className="h-screen bg-slate-900">
-      <div className="text-white text-5xl font-semibold font-serif text-center pt-9 mb-32">
+      <div className={styles.header}>
         PROJECTS
       </div>
       <div className="px-7">
         <Carousel
           responsive={responsive}
-          swipeable={true}
+          swipeable={false}
           draggable={false}
           showDots={false}
           ssr={true} // means to render carousel on server-side.
@@ -45,41 +46,41 @@ export default function Projects() {
           dotListClass="custom-dot-list-style"
           itemClass="carousel-item-padding-40-px"
         >
-          <div className="bg-white h-[75%] m-2 flex flex-col rounded-lg">
+          <div className={styles.imgStyle}>
             <img
               src="https://res.cloudinary.com/dbymvhk8x/image/upload/v1703961560/Screenshot_2023-12-30_203640_qewhot.png"
               className="w-[100%] h-[80%] rounded-3xl"
             />
-            <div>
+            <div className="flex flex-row underline justify-center items-center h-[100%] font-serif font-semibold">
               <Link to={`/`}>
                 View details
               </Link>
               <ImArrowUpRight2 />
             </div>
           </div>
-          <div className="bg-white h-[75%] m-2 flex flex-col rounded-lg">
+          <div className={styles.imgStyle}>
             <img
               src="https://res.cloudinary.com/dbymvhk8x/image/upload/v1703961877/bt_zzlk6o.png"
               className="w-[100%] h-[80%] rounded-3xl"
             />
-            <button
-              type="button"
-              class="font-bold text-xl bg-gradient-to-r from-cyan-600 to-indigo-800 hover:from-pink-900 hover:to-yellow-500  text-white rounded-xl px-2 py-2 m-auto w-[50%] transition-all duration-500"
-            >
-              show
-            </button>
+            <div className="flex flex-row underline justify-center items-center h-[100%] font-serif font-semibold">
+              <Link to={`/`}>
+                View details
+              </Link>
+              <ImArrowUpRight2 />
+            </div>
           </div>
-          <div className="bg-white h-[75%] m-2 flex flex-col rounded-lg">
+          <div className={styles.imgStyle}>
             <img
               src="https://res.cloudinary.com/dbymvhk8x/image/upload/v1703961560/Screenshot_2023-12-30_203640_qewhot.png"
               className="w-[100%] h-[80%] rounded-3xl"
             />
-            <button
-              type="button"
-              class="font-bold text-xl bg-gradient-to-r from-cyan-600 to-indigo-800 hover:from-pink-900 hover:to-yellow-500  text-white rounded-xl px-2 py-2 m-auto w-[50%] transition-all duration-500"
-            >
-              show
-            </button>
+            <div className="flex flex-row underline justify-center items-center h-[100%] font-serif font-semibold">
+              <Link to={`/`}>
+                View details
+              </Link>
+              <ImArrowUpRight2 />
+            </div>
           </div>
         </Carousel>
       </div>
---
+++ src/components/Projects/projectStyles.js
@@ -0,0 +1,5 @@
+exports.styles={
+    header:"text-white text-5xl font-semibold font-serif text-center pt-9 mb-32",
+    imgStyle:"bg-white h-[75%] m-2 flex flex-col rounded-lg",
+    
+}
