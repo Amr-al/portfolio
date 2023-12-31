a5cd069fb53326c7b73d3e45e1a1edc52b48b17e
amr mohamed <amrmohamed09@gmail.com>
2023-12-31 16:30:32 +0200

    adding skills

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from "./components/Nav/Nav";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Projects/>
+      <Skills/>
     </>
   );
 }
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,98 @@
+import React from "react";
+
+export default function Skills() {
+  return (
+    <div className="bg-slate-900 min-h-screen h-fit ">
+      <h1 className="text-white text-5xl font-semibold font-serif text-center mb-20">
+        {" "}
+        SKILLS
+      </h1>
+      <div className="flex flex-row flex-wrap px-10 justify-center">
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+            className="w-1/3 mb-6 m-auto"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl m-auto ">React Js</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-min-h-1/4 w-80 items-center">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg"
+            className="w-1/3 mb-6 m-auto"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl m-auto ">Node Js</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://ajeetchaulagain.com/static/7cb4af597964b0911fe71cb2f8148d64/87351/express-js.png"
+            className="w-1/3 mb-6 m-auto"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl ">Express Js</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://res.cloudinary.com/dbymvhk8x/image/upload/v1704031809/mongodb_1_i5gffq.png"
+            className="w-1/3 mb-6 m-auto"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl "> MongoDB </h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/810px-Postgresql_elephant.svg.png"
+            className="w-1/3 mb-6 m-auto"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl ">PostgreSQL</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Socket-io.svg/1200px-Socket-io.svg.png"
+            className="w-1/3 mb-6 m-auto"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl ">Socket.io</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://e7.pngegg.com/pngimages/439/345/png-clipart-bootstrap-logo-thumbnail-tech-companies.png"
+            className="w-1/3 mb-6 m-auto rounded-full"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl ">Bootstrap</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://w7.pngwing.com/pngs/915/519/png-transparent-typescript-hd-logo-thumbnail.png"
+            className="w-1/3 mb-6 m-auto rounded-full"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl ">Typescript</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://community.pinterest.biz/t5/image/serverpage/image-id/414iC360BC91231556A1/image-size/large/is-moderation-mode/true?v=v2&px=999"
+            className="w-1/3 mb-6 m-auto rounded-full"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl ">Tailwind</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+            className="w-1/3 mb-6 m-auto rounded-full"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl ">GitHub</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://e7.pngegg.com/pngimages/629/262/png-clipart-node-js-object-relational-mapping-javascript-microsoft-sql-server-postgresql-column-blue-angle-thumbnail.png"
+            className="w-1/3 mb-6 m-auto rounded-full"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl ">ORM</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center">
+          <img
+            src="https://p7.hiclipart.com/preview/827/1021/173/2017-acm-international-collegiate-programming-contest-competitive-programming-kateb-university-association-for-computing-machinery-acm-icpc-dhaka-site-others.jpg"
+            className="w-1/3 mb-6 m-auto rounded-full"
+          />
+          <h1 className="text-white font-serif font-semibold text-2xl ">Problem Solving</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
