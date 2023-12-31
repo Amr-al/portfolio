d47fada9f7d6d626f098f7a342bd1a935030c49d
amr mohamed <amrmohamed09@gmail.com>
2023-12-31 16:39:40 +0200

    adding style file

---
+++ src/components/Skills/skillsStyle.js
@@ -0,0 +1,8 @@
+exports.styles={
+    container:"bg-slate-900 min-h-screen h-fit",
+    header:"text-white text-5xl font-semibold font-serif text-center mb-20",
+    imgsContainer:"flex flex-row flex-wrap px-10 justify-center",
+    cardContainer:"flex flex-col justify-center text-center mb-11 min-h-1/4 w-80 items-center",
+    singleImage:"w-1/3 mb-6 m-auto rounded-full",
+    name:"text-white font-serif font-semibold text-2xl m-auto "
+}
