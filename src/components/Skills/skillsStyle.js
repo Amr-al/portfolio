549f9589ad4d205c398d73cec0732f9ce15acee9
amr mohamed <amrmohamed09@gmail.com>
2024-01-01 18:25:22 +0200

    fix responsive in skills

--- src/components/Skills/skillsStyle.js
+++ src/components/Skills/skillsStyle.js
@@ -3,7 +3,7 @@ exports.styles={
     header:"text-white text-5xl font-semibold font-serif text-center mb-12",
     description:"font-serif text-center text-gray-500 m-auto text-xl max-w-[70%] mb-8",
     imgsContainer:"flex flex-row flex-wrap w-full md:w-[80%] m-auto justify-center md:gap-1 ",
-    cardContainer:"flex flex-col justify-center text-center mb-11 min-h-1/4 md:w-1/6 items-center",
+    cardContainer:"flex flex-col justify-center text-center mb-11 min-h-1/2 w-full md:w-1/6 items-center",
     singleImage:"w-1/3 mb-6 m-auto rounded-full hover:p-1 transition-all duration-500",
     name:"text-white font-serif font-semibold text-md m-auto "
 }
