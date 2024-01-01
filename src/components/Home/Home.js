b80110e11d019295b7c7d988ed50d8e7e51f1f94
amr mohamed <amrmohamed09@gmail.com>
2024-01-01 16:09:08 +0200

    changing style of header section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,10 +4,9 @@ import { homeStyle } from "./HomeStyles";
 export default function Home() {
   return (
     <div className={homeStyle.homeContainer} id="home">
-      <h1 className={homeStyle.name}>Hi, I am Amr Mohamed</h1>
-      <h1 className={homeStyle.title}>Full Stack</h1>
-      <h1 className={homeStyle.title}>Software</h1>
-      <h1 className={homeStyle.title}>Developer</h1>
+      <h1 className={homeStyle.name}>HI, I AM AMR MOHAMED</h1>
+      <h1 className={homeStyle.title} >A Full Stack Developer focused Web Developer building Websites and Web Applications that leads to the success of the overall product</h1>
+
       <button type="button" className={homeStyle.btn}>
         Preview Projects
       </button>
