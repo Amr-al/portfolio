03aaa30fde435784bf7491fac415a63407da9fbe
amr mohamed <amrmohamed09@gmail.com>
2024-01-01 17:16:13 +0200

    changing project height

--- src/components/Projects/projectStyles.js
+++ src/components/Projects/projectStyles.js
@@ -1,5 +1,5 @@
 exports.styles={
     header:"text-white text-5xl font-semibold font-serif text-center pt-9 mb-32",
-    imgStyle:"bg-white h-[75%] m-2 flex flex-col rounded-lg",
+    imgStyle:"bg-white h-auto w-[95%] m-2 flex flex-col rounded-lg",
     
 }
